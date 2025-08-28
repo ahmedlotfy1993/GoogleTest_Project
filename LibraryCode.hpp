fa74356beaba430e4560b066657056e13cbbebef
Ahmed Lotfy <ahmedlotfy271993@gmail.com>
2025-08-28 17:37:19 +0200

    Update LibraryCode.hpp

--- LibraryCode.hpp
+++ LibraryCode.hpp
@@ -1,2 +1,21 @@
 #pragma once
+#include <vector>
 int sum(int a, int b);
+bool isPositive(int num);
+int CountPositives(const std::vector<int>& vec);
+class Account{
+	double mBalance;
+	public:
+		Account();
+		void deposit(double sum);
+		void withdraw(double sum);
+		double getBalance()const;
+		void transfer(Account& to, double sum);
+};
+class Validator{
+	int mLow;
+	int mHigh;
+	public:
+		Validator(int low, int high);
+		bool InRange(int val);
+};
