98b1f306ca160f38e0026b58678a48e40ffabd64
Ahmed Lotfy <ahmedlotfy271993@gmail.com>
2025-08-28 17:38:46 +0200

    Update LibraryCode.cpp

--- LibraryCode.cpp
+++ LibraryCode.cpp
@@ -1,5 +1,34 @@
 #include "LibraryCode.hpp"
-
+#include <algorithm>
+#include <stdexcept>
 int sum(int a, int b){
 	return a+b;
+}
+bool isPositive(int num){
+	return num >= 0;
+}
+int CountPositives(const std::vector<int>& vec){
+	return std::count_if(vec.begin(), vec.end(), isPositive);
+}
+Account::Account(): mBalance(0){}
+void Account::deposit(double sum){
+	mBalance += sum;
+}
+void Account::withdraw(double sum){
+	if(mBalance < sum){
+		throw std::runtime_error("Insufficient error");
+	}
+	mBalance -= sum;
+}
+double Account::getBalance()const{
+	return mBalance;
+}
+void Account::transfer(Account& to, double sum){
+	this->withdraw(sum);
+	to.deposit(sum);
+}
+Validator::Validator(int low, int high):mLow(low), mHigh(high){
+}
+bool Validator::InRange(int val){
+	return val >= mLow && val <= mHigh;
 }
